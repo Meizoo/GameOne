a50464414d69fcd1d7f73719d0cc70584f182cce
Patryk Milewski <mpatryk97@gmail.com>
2017-06-27 18:47:13 +0200

    Reorganization

--- GameOne/GameOne/Entity.cs
+++ GameOne/GameOne/Entity.cs
@@ -5,104 +5,36 @@ using System.Text;
 using System.Threading.Tasks;
 
 using Microsoft.Xna.Framework;
-// Struct, Entity, mockups of Player, Monster, Npc
+
 namespace GameOne
 {
 	abstract class Entity
 	{
-		private string Name;
-
-		private bool IsVisible;
-		private bool IsPushable;
-		private bool IsTargetable;
-
-		private bool IsOnGround;
-
-		private Gravity Gravity;
-		private Description Description;
-		private Velocity Velocity;
-		private Size Size;
-		private Vector2 Position;
-	}
-
-	struct Gravity
-	{
-		public Gravity(double current, double acceleration, double maximal = MaximalDefault)
+		public Entity(
+				string name,
+				Flags flags,
+				Gravity gravity,
+				Description description,
+				Velocity velocity,
+				Size size,
+				Vector2 position
+			)
 		{
-			this.Maximal = maximal;
-			this.Acceleration = acceleration;
-			this.Current = current;
+			this.Name = name;
+			this.Flags = flags;
+			this.Gravity = gravity;
+			this.Description = description;
+			this.Velocity = velocity;
+			this.Size = size;
+			this.Position = position;
 		}
 
-		public double Maximal;
-		public double Current;
-		public double Acceleration;
-
-		private const double MaximalDefault = 128;
-	}
-
-	struct Description
-	{
-		public Description(string shortDesc, string longDesc)
-		{
-			this.Short = shortDesc;
-			this.Long = longDesc;
-		}
-
-		public string Short;
-		public string Long;
-	}
-
-	struct Velocity
-	{
-		public Velocity(double current, double minimal = DefaultMinimal, double maximal = DefaultMaximal)
-		{
-			this.Maximal = maximal;
-			this.Current = current;
-			this.Minimal = minimal;
-		}
-
-		public double Maximal;
-		public double Current;
-		public double Minimal;
-
-		private const double DefaultMaximal = 128;
-		private const double DefaultMinimal = 0;
-	}
-
-	struct Size
-	{
-		public Size(int width, int height)
-		{
-			this.Width = width;
-			this.Height = height;
-		}
-
-		public int Width;
-		public int Height;
-	}
-
-	class Player : Entity
-	{
-
-
-
-		private bool HasJumped;
-		private double JumpVelocity;
-	}
-
-	class Monster : Entity
-	{
-
-	}
-
-	class NPC : Entity
-	{
-
-	}
-
-	class Item
-	{
-
+		protected string Name;
+		protected Flags Flags;
+		protected Gravity Gravity;
+		protected Description Description;
+		protected Velocity Velocity;
+		protected Size Size;
+		protected Vector2 Position;
 	}
 }
