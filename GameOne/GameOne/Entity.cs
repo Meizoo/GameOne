fca360cd7428f4c76380386ef7f6502972ac266d
Meizoo <dawidek199710@gmail.com>
2017-06-27 18:54:50 +0200

    moze wejdzie

--- GameOne/GameOne/Entity.cs
+++ GameOne/GameOne/Entity.cs
@@ -118,6 +118,41 @@ namespace GameOne
     {
 
     }
+=======
+=======
+>>>>>>> a50464414d69fcd1d7f73719d0cc70584f182cce
+
+namespace GameOne
+{
+	abstract class Entity
+	{
+		public Entity(
+				string name,
+				Flags flags,
+				Gravity gravity,
+				Description description,
+				Velocity velocity,
+				Size size,
+				Vector2 position
+			)
+		{
+			this.Name = name;
+			this.Flags = flags;
+			this.Gravity = gravity;
+			this.Description = description;
+			this.Velocity = velocity;
+			this.Size = size;
+			this.Position = position;
+		}
+
+		protected string Name;
+		protected Flags Flags;
+		protected Gravity Gravity;
+		protected Description Description;
+		protected Velocity Velocity;
+		protected Size Size;
+		protected Vector2 Position;
+	}
 <<<<<<< HEAD
 >>>>>>> a50464414d69fcd1d7f73719d0cc70584f182cce
 =======
