40441750c46f1cc8f2ee7fe360407d323bc7aacb
Meizoo <dawidek199710@gmail.com>
2017-06-27 17:53:47 +0200

    Add an entity class

---
+++ GameOne/GameOne/Entity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+namespace GameOne
+{
+    abstract class Entity
+    {
+
+        private Vector2 Position;
+    }
+}
